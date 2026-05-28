f3f77de4970fd19791c3f5e86d03332104551576
saav15 <saavtad@gmail.com>
2026-05-28 01:16:06 -0600

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,13 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# Usamos SQLite guardando el archivo en /app/data que será un volumen de Docker
-DATABASE_URL = "sqlite:////app/data/markettrust.db"
+# Usamos SQLite guardando en un archivo v2 para forzar la creación de la nueva columna 'description'
+DATABASE_URL = "sqlite:///./markettrust_v2.db"
 
-# En caso de que se ejecute localmente sin Docker, aseguramos que exista
-os.makedirs("/app/data", exist_ok=True)
-# Alternativa para local dev: 
-# DATABASE_URL = "sqlite:///./markettrust.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
